87d96b0178ec8a63710fc57271420c932384b4eb
Ryan Morrison-Westphal <ryanmw@amazon.com>
2020-12-18 18:11:45 -0500

    typo

---
+++ js/mmhome.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+
+  if ('serviceWorker' in navigator) {
+    navigator
+      .serviceWorker
+      .register('sw.js')
+      .then( function(reg) {
+        console.log('Registration successful');
+      });
+  }
+} );
