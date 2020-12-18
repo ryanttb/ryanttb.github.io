40be8212eae4d80925ea8bd8e68afb3c1487d199
Ryan Morrison-Westphal <ryanmw@amazon.com>
2020-12-18 18:28:31 -0500

    worker path

--- js/myhome.js
+++ js/myhome.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
   if ('serviceWorker' in navigator) {
     navigator
       .serviceWorker
-      .register('sw.js')
+      .register('js/sw.js')
       .then( function(reg) {
         console.log('Registration successful');
       });
